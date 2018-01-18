51c95b18fe9b18ab18e6c902e3afbfc37b871143
kevwargo <jarasz.kevwargo@gmail.com>
2018-01-18 12:11:12 +0100

    Log klipper execution

--- klipper.lisp
+++ klipper.lisp
@@ -1,3 +1,3 @@
-(run-shell-command "klipper")
-
-(define-key *top-map* (kbd "H-K") "exec qdbus org.kde.klipper /klipper showKlipperPopupMenu")
+(let ((klipper-log (data-dir-file "klipper" "log")))
+  (run-shell-command (format nil "klipper >> ~A 2>&1" klipper-log))
+  (define-key *top-map* (kbd "H-K") (format nil "exec qdbus org.kde.klipper /klipper showKlipperPopupMenu >> ~A 2>&1" klipper-log)))

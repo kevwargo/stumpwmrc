30c5271b0b754f10db01cf8aca8335b3d3564684
jarasz.lojka <jarasz.lojka@mindseater.com>
2018-04-09 09:41:11 +0200

    Start konsole in homedir

--- konsole.sh
+++ konsole.sh
@@ -3,9 +3,9 @@
 cd "$(dirname "$(realpath "$0")")"
 
 if [ -x ./konsole/build/src/konsole ]; then
-    ./konsole/build/src/konsole "$@"
+    ./konsole/build/src/konsole --workdir ~ "$@"
 elif [ -x "/mnt/develop/my/cpp/konsole/build/src/konsole" ]; then
-    /mnt/develop/my/cpp/konsole/build/src/konsole "$@"
+    /mnt/develop/my/cpp/konsole/build/src/konsole --workdir ~ "$@"
 else
     echo "No konsole found."
     exit 1

d6277d1f5e7a92435913adc55423ebfc8b8249c7
kevwargo <jarasz.kevwargo@gmail.com>
2018-06-07 11:51:49 +0200

    Test version of amixer volume in mode-line

--- mode-line.lisp
+++ mode-line.lisp
@@ -18,6 +18,27 @@
     (dolist (head (screen-heads screen))
       (enable-mode-line screen head nil))))
 
+(defun amixer-get-master-volume (mode-line)
+  (declare (ignore mode-line))
+  ;; (let ((process (sb-ext:run-program "/usr/bin/strace"
+  ;;                                    (list "-o" "/home/jarasz/amixer-strace.log" "/usr/bin/amixer" "-M" "-c" "0" "get" "Master")
+  ;; (let ((process (sb-ext:run-program "/usr/bin/amixer"
+  (let ((process (sb-ext:run-program "/mnt/develop/soft/sources/c/alsa-utils-1.1.2/amixer/amixer"
+                                     (list "-M" "-c" "0" "get" "Master")
+                                     :output :stream
+                                     :wait nil
+                                     :environment (sb-ext:posix-environ)))
+        line)
+    (quicklog "amixer process started")
+    (loop while (setf line (read-line (sb-ext:process-output process) nil))
+       do (multiple-value-bind (matched groups)
+              (cl-ppcre:scan-to-strings "([0-9]+)%" line)
+            (if matched
+                (return (svref groups 0)))))))
+    ;; "asdf"))
+
+(add-screen-mode-line-formatter #\A 'amixer-get-master-volume)
+
 (define-key *root-map* (kbd "m") "mode-line-all-heads-on")
 (define-key *root-map* (kbd "M") "mode-line-all-heads-off")
 

dace6a91e1d772de9ca228766a1d11ed58d8c064
jarasz_lojka <jaras.lojka@nordcloud.com>
2018-10-16 12:07:40 +0200

    Command to set env vars

--- vars.lisp
+++ vars.lisp
@@ -1,4 +1,8 @@
+(defcommand set-env-var (var val)
+    ((:string "Variable: ")
+     (:string "Value: "))
+  (setf (getenv var) val)
+  (message "Env var '~A' set to '~A'" var val))
+
 (setf *input-history-ignore-duplicates* t)
 (setf *mouse-focus-policy* :ignore)
-
-(setf (getenv "QT_SCALE_FACTOR") "1.8")

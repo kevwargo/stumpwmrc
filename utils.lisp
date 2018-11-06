7ef9110b2a2cbdb32e395deb292d4721937622de
kevwargo <jarasz.kevwargo@gmail.com>
2018-11-06 03:19:15 +0100

    run-prog-collect-lines util

--- utils.lisp
+++ utils.lisp
@@ -5,4 +5,18 @@
     (prog1
         (and (characterp c) (char= c #\y))
       (unmap-all-message-windows))))
-      
+
+(defun run-prog-collect-lines (prog &rest opts &key args wait &allow-other-keys)
+  (remf opts :args)
+  (remf opts :wait)
+  (let ((proc (apply #'sb-ext:run-program prog args
+                     :output :stream
+                     :wait nil
+                     opts)))
+    (prog1
+        (with-open-stream (stream (sb-ext:process-output proc))
+          (loop with line
+             while (setq line
+                         (read-line stream nil nil))
+             collect line))
+      (if wait (sb-ext:process-wait proc)))))
